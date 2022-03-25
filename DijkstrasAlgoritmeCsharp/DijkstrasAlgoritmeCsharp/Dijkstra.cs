3f59b9a63aaefdabaf64b2bcda4b65c4a2e51bb4
Erik Langendijk <erik.langendijk@gmail.com>
2022-03-25 12:24:38 +0100

    Refactored code

--- DijkstrasAlgoritmeCsharp/DijkstrasAlgoritmeCsharp/Dijkstra.cs
+++ DijkstrasAlgoritmeCsharp/DijkstrasAlgoritmeCsharp/Dijkstra.cs
@@ -23,29 +23,54 @@ namespace DijkstrasAlgoritmeCsharp
             _graph = graph;
         }
 
+        /// <summary>
+        /// Find the node with the lowest cost available and return its index
+        /// </summary>
+        /// <param name="nodeCosts">Array containing the costs to each available node</param>
+        /// <param name="visited">Array containing visited nodes</param>
+        /// <returns></returns>
+        public int findLowestCost(int index, bool[] visitedNodes)
+        {
+            int lowestCost = int.MaxValue; // int which holds the lowest cost found
+            int node_index = -1; // int which holds the index of the node with lowest cost
+
+            // iterate trough graph node containing the costs to other nodes
+            for (int i = 0; i < _graph.GetLength(1); i++)
+            {
+                // check if the current node is connected (not 0) and has a lower cost
+                if (visitedNodes[i] == false && _graph[index, i] > 0 && _graph[index, i] <= lowestCost)
+                {
+                    lowestCost = _graph[index, i];
+                    node_index = i;
+                }
+            }
+
+            return node_index;
+        }
+
         public void search()
         {
-            // init empty arrays for the calculated distances and visited nodes
-            int sizeGraph = _graph.GetLength(1);
-            int[] distances = new int[sizeGraph];
-            bool[] visitedNodes = new bool[sizeGraph];
+            int amountNodes = _graph.GetLength(0); // get amount of nodes
+            int[] distances = new int[amountNodes]; // init new array of distances to return
+            bool[] visited = new bool[amountNodes]; // init new array of visited nodes
 
-            // initialise the distances array with the largest number it can hold and set the visited array all to false
-            for (int i = 0; i < sizeGraph; i++)
+            // set the distances of all nodes to the highest value and all nodes to unvisited
+            for (int i = 0; i < amountNodes; i++)
             {
                 distances[i] = int.MaxValue;
-                visitedNodes[i] = false;
-            }    
+                visited[i] = false;
+            }
 
-            distances[_source] = 0; // set the start node to 0
+            distances[_source] = 0; // the distance of the first node to itself is always zero
 
-            // loop trough the list of nodes, but skip the first node
-            for (int i = 0; i <= sizeGraph - 1; i++)
+            for (int i = 0; i < amountNodes; i++)
             {
-                // set current node as visited
-                visitedNodes[i] = true;
+                // find the node with the lowest cost
+                int lowestIndex = findLowestCost(i, visited);
 
+                // set this node to visited
+                visited[lowestIndex] = true;
             }
         }
-}
     }
+}
